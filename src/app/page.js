1ee1a87f303f92f5f4956c10bd6e4fe3e3525f55
Arondeep Nishand <nttr2023@gmail.com>
2026-02-02 17:14:00 -0800

    Update service area locations

--- src/app/page.js
+++ src/app/page.js
@@ -23,8 +23,7 @@ const BUSINESS = {
     { day: "Sat", hours: "9:00 AM – 4:00 PM" },
     { day: "Sun", hours: "Closed" },
   ],
-  serviceArea: ["TEST UPDATE - REMOVE LATER",
-"West Sacramento", "Sacramento", "Woodland", "Davis", "Elk Grove", "Natomas", "Roseville", "Vacaville","Galt", "Lodi","Rancho Cordova"],
+  serviceArea: ["West Sacramento", "Sacramento", "Woodland", "Davis", "Elk Grove", "Natomas", "Roseville", "Vacaville","Galt", "Lodi","Rancho Cordova"],
 };
 
 const SERVICES = [
